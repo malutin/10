0a38cd82afaba164c0c60c6d73d0eb0dc07c7e5e
MMA <lesnic1987@mail.ru>
2022-08-29 21:33:27 +0300

    Задача 10

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.Clear();
+// Console.WriteLine("Введите трехзначное число");
+// string a = Console.ReadLine();
+// Console.WriteLine(a[1]);
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 10;
+b = b % 10;
+Console.WriteLine(b);
